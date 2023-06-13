470f3dcbb7c5ab2544c35855c25fecae6cd77660
Anoop Pandey <75595666+gargdev@users.noreply.github.com>
2023-06-14 01:43:05 +0530

    Update Navbars.jsx links

--- src/components/Navbars.jsx
+++ src/components/Navbars.jsx
@@ -1,308 +1,17 @@
-// import React, { useState } from "react";
-// import Logo from "../assets/Logo1.png";
-// import { Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemText, Menu, MenuItem, useMediaQuery } from "@mui/material";
-// import { Link } from "react-router-dom";
-// import { Fade } from "@mui/material";
-// import MenuIcon from "@mui/icons-material/Menu";
-// import "./navbars.css";
-// import { Drawer } from "@mui/material";
-// const Navbar = () => {
-//   const [openMenu, setOpenMenu] = useState(false);
-//   const [openDropdown, setOpenDropdown] = useState(false);
-//   const [selectedOption, setSelectedOption] = useState("");
-//   const menuOptions = [
-//     {
-//       text: "Home",
-//     },
-//     {
-//       text: "About",
-//     },
-//     {
-//       text: "Offerings",
-//       dropdown: true,
-//       options: [
-//         "Mentor Connect",
-//         "Investor Connect",
-//         "Grant Connect",
-//       ],
-//     },
-//     {
-//       text: "Service",
-//     },
-//     {
-//       text: "Contact",
-//     },
-//   ];
-
-//   const isMobile = useMediaQuery("(max-width: 768px)");
-
-//   const handleOpenDropdown = () => {
-//     setOpenDropdown(true);
-//   };
-
-//   const handleCloseDropdown = () => {
-//     setOpenDropdown(false);
-//   };
-
-//   const handleOptionSelect = (option) => {
-//     setSelectedOption(option);
-//     handleCloseDropdown();
-//   };
-
-//   return (
-//     <nav>
-//       <div className="navbar-logo-container">
-//         <Link to="/">
-//           <img src={Logo} alt="logo" />
-//         </Link>
-//       </div>
-//       <div className="navbar-links-container">
-//         {menuOptions.map((item) =>
-//           item.dropdown ? (
-//             <div
-//               key={item.text}
-//               className="navbar-dropdown"
-//               onMouseEnter={handleOpenDropdown}
-//               onMouseLeave={handleCloseDropdown}
-//             >
-//               <Link to="/offerings">{item.text}</Link>
-//               {openDropdown && (
-//                 <Menu
-//                   anchorEl={document.querySelector(".navbar-dropdown")}
-//                   open={openDropdown}
-//                   onClose={handleCloseDropdown}
-//                   MenuListProps={{
-//                     "aria-labelledby": "dropdown-offerings",
-//                   }}
-//                 >
-//                   {item.options.map((option) => (
-//                     <MenuItem
-//                       key={option}
-//                       onClick={() => handleOptionSelect(option)}
-//                     >
-//                       {option}
-//                     </MenuItem>
-//                   ))}
-//                 </Menu>
-//               )}
-//             </div>
-//           ) : (
-//             <Link key={item.text} to="/">
-//               {item.text}
-//             </Link>
-//           )
-//         )}
-//         <Link to="/register">
-//           <button className="primary-button">Join Now</button>
-//         </Link>
-//       </div>
-
-//       {isMobile ? (
-//         <div className="navbar-menu-container">
-//           <IconButton onClick={() => setOpenMenu(true)}>
-//             <MenuIcon />
-//           </IconButton>
-//         </div>
-//       ) : (
-//         <div className="navbar-menu-container">
-//           <IconButton onClick={() => setOpenMenu(true)}>
-//             <MenuIcon />
-//           </IconButton>
-//         </div>
-//       )}
-
-//       <Drawer
-//         open={openMenu}
-//         onClose={() => setOpenMenu(false)}
-//         anchor="right"
-//       >
-//         <Fade in={openMenu}>
-//           <Box sx={{ width: 250 }}>
-//             <List>
-//               {menuOptions.map((item) => (
-//                 <ListItem key={item.text} disablePadding>
-//                   {item.button ? (
-//                     <ListItemButton>
-//                       <button className="primary-button">{item.text}</button>
-//                     </ListItemButton>
-//                   ) : (
-//                     <ListItemButton component={Link} to={`/${item.text.toLowerCase()}`}>
-//                       <ListItemText primary={item.text} />
-//                     </ListItemButton>
-//                   )}
-//                 </ListItem>
-//               ))}
-//             </List>
-//             <Divider />
-//           </Box>
-//         </Fade>
-//       </Drawer>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
-
-// import React, { useState } from "react";
-// import Logo from "../assets/Logo1.png";
-// import { Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemText, Menu, MenuItem, useMediaQuery } from "@mui/material";
-// import { Link } from "react-router-dom";
-// import { Fade } from "@mui/material";
-// import MenuIcon from "@mui/icons-material/Menu";
-// import "./navbars.css";
-// import { Drawer } from "@mui/material";
-
-// const Navbar = () => {
-//   const [openMenu, setOpenMenu] = useState(false);
-//   const [openDropdown, setOpenDropdown] = useState(false);
-//   const [selectedOption, setSelectedOption] = useState("");
-//   const menuOptions = [
-//     {
-//       text: "Home",
-//     },
-//     {
-//       text: "About",
-//     },
-//     {
-//       text: "Offerings",
-//       dropdown: true,
-//       options: [
-//         "Mentor Connect",
-//         "Investor Connect",
-//         "Grant Connect",
-//       ],
-//     },
-//     {
-//       text: "Service",
-      
-//     },
-//     {
-//       text: "Contact",
-//     },
-//   ];
-
-//   const isMobile = useMediaQuery("(max-width: 768px)");
-
-//   const handleOpenDropdown = () => {
-//     setOpenDropdown(true);
-//   };
-
-//   const handleCloseDropdown = () => {
-//     setOpenDropdown(false);
-//   };
-
-//   const handleOptionSelect = (option) => {
-//     setSelectedOption(option);
-//     handleCloseDropdown();
-//   };
-
-//   return (
-//     <nav className="flex items-center justify-between  px-4 py-2 ">
-//       <div className="navbar-logo-container">
-//         <Link to="/">
-//           <img src={Logo} alt="logo" className="w-10 h-auto" />
-//         </Link>
-//       </div>
-//       <div className="navbar-links-container">
-//         {menuOptions.map((item) =>
-//           item.dropdown ? (
-//             <div
-//               key={item.text}
-//               className="navbar-dropdown relative"
-//               onMouseEnter={handleOpenDropdown}
-//               onMouseLeave={handleCloseDropdown}
-//             >
-//               <Link to="/offerings" className="text-gray-800 hover:text-blue-500">
-//                 {item.text}
-//               </Link>
-//               {openDropdown && (
-//                 <Menu
-//                   anchorEl={document.querySelector(".navbar-dropdown")}
-//                   open={openDropdown}
-//                   onClose={handleCloseDropdown}
-//                   MenuListProps={{
-//                     "aria-labelledby": "dropdown-offerings",
-//                   }}
-//                 >
-//                   {item.options.map((option) => (
-//                     <MenuItem
-//                       key={option}
-//                       onClick={() => handleOptionSelect(option)}
-//                     >
-//                       {option}
-//                     </MenuItem>
-//                   ))}
-//                 </Menu>
-//               )}
-//             </div>
-//           ) : (
-//             <Link
-//               key={item.text}
-//               to="/"
-//               className="text-gray-800 hover:text-blue-500"
-//             >
-//               {item.text}
-//             </Link>
-//           )
-//         )}
-//         <Link to="/register">
-//           <button className="primary-button">Join Now</button>
-//         </Link>
-//       </div>
-
-//       {isMobile ? (
-//         <div className="navbar-menu-container">
-//           <IconButton onClick={() => setOpenMenu(true)}>
-//             <MenuIcon />
-//           </IconButton>
-//         </div>
-//       ) : (
-//         <div className="navbar-menu-container">
-//           <IconButton onClick={() => setOpenMenu(true)}>
-//             <MenuIcon />
-//           </IconButton>
-//         </div>
-//       )}
-
-//       <Drawer
-//         open={openMenu}
-//         onClose={() => setOpenMenu(false)}
-//         anchor="right"
-//       >
-//         <Fade in={openMenu}>
-//           <Box sx={{ width: 250 }}>
-//             <List>
-//               {menuOptions.map((item) => (
-//                 <ListItem key={item.text} disablePadding>
-//                   {item.button ? (
-//                     <ListItemButton>
-//                       <button className="primary-button">{item.text}</button>
-//                     </ListItemButton>
-//                   ) : (
-//                     <ListItemButton
-//                       component={Link}
-//                       to={`/${item.text.toLowerCase()}`}
-//                     >
-//                       <ListItemText primary={item.text} />
-//                     </ListItemButton>
-//                   )}
-//                 </ListItem>
-//               ))}
-//             </List>
-//             <Divider />
-//           </Box>
-//         </Fade>
-//       </Drawer>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import React, { useState } from "react";
 import Logo from "../assets/Logo1.png";
-import { Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemText, Menu, MenuItem, useMediaQuery } from "@mui/material";
+import {
+  Box,
+  Divider,
+  IconButton,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Menu,
+  MenuItem,
+  useMediaQuery,
+} from "@mui/material";
 import { Link } from "react-router-dom";
 import { Fade } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
@@ -330,12 +39,11 @@ const Navbar = () => {
       text: "Services",
       dropdown: true,
       options: [
-        "Service 1",
-        "Service 2",
-        "Service 3",
-        "Service 4",
-        "Service 5",
-        "Service 6",
+        "Business Canvas",
+        "Financial Modelling",
+        "Pitch Deck creation",
+        "Investor ready Kit",
+        "Startup Tech Kit",
       ],
     },
     {
@@ -347,6 +55,7 @@ const Navbar = () => {
 
   const handleOpenOfferingsDropdown = () => {
     setOpenOfferingsDropdown(true);
+    setOpenServicesDropdown(false);
   };
 
   const handleCloseOfferingsDropdown = () => {
@@ -355,6 +64,7 @@ const Navbar = () => {
 
   const handleOpenServicesDropdown = () => {
     setOpenServicesDropdown(true);
+    setOpenOfferingsDropdown(false);
   };
 
   const handleCloseServicesDropdown = () => {
@@ -380,10 +90,18 @@ const Navbar = () => {
             <div
               key={item.text}
               className="navbar-dropdown relative"
-              onMouseEnter={item.text === "Offerings" ? handleOpenOfferingsDropdown : handleOpenServicesDropdown}
-              onMouseLeave={item.text === "Offerings" ? handleCloseOfferingsDropdown : handleCloseServicesDropdown}
+              onMouseEnter={
+                item.text === "Offerings"
+                  ? handleOpenOfferingsDropdown
+                  : handleOpenServicesDropdown
+              }
+              // onMouseLeave={
+              //   item.text === "Offerings"
+              //     ? handleCloseOfferingsDropdown
+              //     : handleCloseServicesDropdown
+              // }
             >
-              <Link to={item.text === "Offerings" ? "/offerings" : "/services"} className="text-gray-800 hover:text-blue-500">
+              <Link className="text-gray-800 cursor-pointer hover:text-blue-500">
                 {item.text}
               </Link>
               {item.text === "Offerings" && openOfferingsDropdown && (
@@ -400,7 +118,19 @@ const Navbar = () => {
                       key={option}
                       onClick={() => handleOptionSelect(option)}
                     >
-                      {option}
+                      <Link
+                        to={
+                          option === "Mentor Connect"
+                            ? "/mentor-connect"
+                            : option === "Investor Connect"
+                            ? "/investor-connect"
+                            : option === "Grant Connect"
+                            ? "/grant-connect"
+                            : "/offerings"
+                        }
+                      >
+                        {option}
+                      </Link>
                     </MenuItem>
                   ))}
                 </Menu>
@@ -428,7 +158,13 @@ const Navbar = () => {
           ) : (
             <Link
               key={item.text}
-              to="/"
+              to={
+                item.text === "About"
+                  ? "/about"
+                  : item.text === "Contact"
+                  ? "/contact"
+                  : "/"
+              }
               className="text-gray-800 hover:text-blue-500"
             >
               {item.text}
@@ -454,11 +190,7 @@ const Navbar = () => {
         </div>
       )}
 
-      <Drawer
-        open={openMenu}
-        onClose={() => setOpenMenu(false)}
-        anchor="right"
-      >
+      <Drawer open={openMenu} onClose={() => setOpenMenu(false)} anchor="right">
         <Fade in={openMenu}>
           <Box sx={{ width: 250 }}>
             <List>
