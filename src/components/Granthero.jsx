0353d1533d4ef15b55d2033e5b01a86e2b25f92e
Anoop Pandey <75595666+gargdev@users.noreply.github.com>
2023-06-20 02:12:50 +0530

    Update Granthero.jsx

--- src/components/Granthero.jsx
+++ src/components/Granthero.jsx
@@ -4,18 +4,17 @@ import "./granthero.css";
 
 const Granthero = () => {
   return (
-    <div className="grant-parent-container">
+    <div className="grant-parent-container mt-20">
         <div className="grant-content">
             <div className="grant-left-content">
             <div className="grant-heading">
               <h1>
-                Fuel Your Startup: Apply for Our <span>Startup Grants!</span>
+                Fuel Your Startup: Apply for Our <span className="blue-text">Startup Grants!</span>
               </h1>
             </div>
-            <div className="grant-primary-text">
-              <p>Looking to give your startup the boost it needs? </p>
-
+            <div className="grant-primary-text">          
               <p>
+              Looking to give your startup the boost it needs? 
                 Our grants can provide the fuel your business needs to succeed.
                 Our application process is straightforward and our team is ready
                 to help you every step of the way. Don't miss out on this
@@ -23,7 +22,7 @@ const Granthero = () => {
               </p>
             </div>
             <div className="grant-btn-container">
-              <button className="gbtn">Learn More</button>
+              <button className="gbtn p-4 w-32 hover:bg-black text-white">Learn More</button>
             </div>
             </div>
             <div className="grant-right-content">
