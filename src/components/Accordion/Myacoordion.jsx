b02cc2dd9faa816b92ff9274b90e887c0d2b5a84
Anoop Pandey <75595666+gargdev@users.noreply.github.com>
2023-06-22 14:03:44 +0530

    Update Myacoordion.jsx

--- src/components/Accordion/Myacoordion.jsx
+++ src/components/Accordion/Myacoordion.jsx
@@ -1,27 +1,3 @@
-// import React, {useState} from 'react'
-// import "./accordion.css";
-// const Myacoordion = ({question, answer }) => {
-//     const [show, setShow] = useState(false);
-//   return (
-//     <>
-
-//     <div className="faq-parent-container">
-//     <div className="main-heading">
-
-//        <h5>{question}</h5>
-//        <p onClick={()=> setShow(!show)}> {show?  "–": "+"} </p>
-//    </div>
-//    {
-//      show &&  <p className='answer'>{answer}</p>
-//    }
-
-//     </div>
-
-//     </>
-//   )
-// }
-
-// export default Myacoordion
 
 import React, { useState } from "react";
 import "./accordion.css";
@@ -35,15 +11,14 @@ const Myacoordion = ({ question, answer }) => {
       <div className="faq-parent-container">
         <div className="main-heading">
           <Fade right distance="20%" delay={100} duration={1000}>
-            <h5 onClick={() => setShow(!show)} className="text-lg md:text-xl">{question}</h5>
+            <h5 onClick={() => setShow(!show)} className="text-lg md:text-xl cursor-pointer hover:text-blue-500 scale-x-110	 ">{question}</h5>
             {/* <p
               onClick={() => setShow(!show)}
               className="cursor-pointer space-2"
              >
             
               <div className="plus-minus">{show ? "–" : "+"}</div>
-            </p> */}
-            {/* <hr /> */}
+            </p>  */}
           </Fade>
         </div>
         {show && <p className="answer">{answer}</p>}
