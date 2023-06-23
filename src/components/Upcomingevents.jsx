a356b6732723928eebdb4b05dfa76cfdd6b3a3df
Anoop Pandey <75595666+gargdev@users.noreply.github.com>
2023-06-23 15:33:16 +0530

    Update Upcomingevents.jsx added latest events

--- src/components/Upcomingevents.jsx
+++ src/components/Upcomingevents.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import eventimg1 from "../assets/eventimg1.png";
+// import eventimg1 from "../assets/eventimg1.png";
 import "./upcomingevents.css";
 import imgh1 from "../assets/Group 12.png";
 import imgh2 from "../assets/Group 13.png";
@@ -23,28 +23,50 @@ const Upcomingevents = () => {
         </div>
 
         <div className="event-card-container flex flex-wrap flex-row">
-          <img
-            className="w-70 transform scale-100 hover:scale-105"
-            src={imgh1}
-            alt=""
-            class="img-fluid"
-          />
-          <img
-            className="w-70 transform scale-100 hover:scale-105"
-            src={imgh2}
-            alt=""
-            class="img-fluid"
-          />
-          <img
-            className="w-70 transform scale-100 hover:scale-105"
-            src={imgh3}
-            alt=""
-            class="img-fluid"
-          />
+          <a
+            href="https://www.linkedin.com/feed/update/urn:li:activity:7076850441052958720/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="w-70 transform scale-100 hover:scale-105"
+              src={imgh1}
+              alt=""
+              // class="img-fluid"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/co-create-lab/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="w-70 transform scale-100 hover:scale-105"
+              src={imgh2}
+              alt=""
+              // class="img-fluid"
+            />
+          </a>
+          <a
+            href="https://www.linkedin.com/company/co-create-lab/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <img
+              className="w-70 transform scale-100 hover:scale-105"
+              src={imgh3}
+              alt=""
+              // class="img-fluid"
+            />
+          </a>
         </div>
 
         <div className="events-button-container">
-          <button className="loadmore">Learn More</button>
+        <a
+            href="https://twitter.com/cocreatelabs1"
+            target="_blank"
+            rel="noreferrer"
+          ><button className="loadmore">Learn More</button></a>
         </div>
       </div>
     </div>
