b89b0058c8fd1d9488b316eaa278eb247610acc9
Anoop Pandey <75595666+gargdev@users.noreply.github.com>
2023-06-23 23:13:18 +0530

    Update App.jsx updated services route

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,3 @@
-// import React from "react";
-// import Navbars from "./components/Navbars";
-// import "./App.css";
-// import { Landing } from "./pages/Landing";
-// import { Mentors } from "./pages/Mentors";
-// import { Mentor } from "./pages/Mentor";
-// import { Investor } from "./pages/Investor";
-// import { Connect } from "./pages/Connect";
-// import { About } from "./pages/About";
-// // import { SignUp } from "./pages/SignUp";
-// import { Routes, Route } from "react-router-dom";
-// import Footer from "./components/Footer";
-// import Fade from "react-reveal/Fade";
-// import Flip from "react-reveal/Flip";
-// const App = () => {
-//   return (
-//     <>
-//       <Navbars />
-//       <flip top distance="10%" duration={1000} delay={500}>
-//         <Routes>
-//           <Route exact path="/" element={<Landing />} />
-//           <Route exact path="/about" element={<About />} />
-//           {/* <Route exact path="/register" element={<SignUp />} /> */}
-//           <Route exact path="/mentor" element={<Mentors />} />
-//           <Route exact path="/mentor-connect" element={<Mentor />} />
-//           <Route exact path="/investor-connect" element={<Investor />} />
-//           <Route exact path="/grant-connect" element={<Connect />} />
-//         </Routes>
-//       </flip>
-//       <Footer />
-//     </>
-//   );
-// };
-
-// export default App;
-
 import React from "react";
 import Navbars from "./components/Navbars";
 import "./App.css";
@@ -43,11 +7,12 @@ import { Mentor } from "./pages/Mentor";
 import { Investor } from "./pages/Investor";
 import { Connect } from "./pages/Connect";
 import { About } from "./pages/About";
-// import { SignUp } from "./pages/SignUp";
+import {Services} from "./pages/Services";
+// import { SignUp } from "./pages/signUp";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/Footer";
 import Fade from "react-reveal/Fade";
-import Flip from "react-reveal/Flip";
+// import Flip from "react-reveal/Flip";
 const App = () => {
   return (
     <>
@@ -65,6 +30,7 @@ const App = () => {
           <Route exact path="/mentor-connect" element={<Mentor />} />
           <Route exact path="/investor-connect" element={<Investor />} />
           <Route exact path="/grant-connect" element={<Connect />} />
+          <Route exact path="/services" element={<Services />} />
         </Routes>
         <Footer />
       </div>
