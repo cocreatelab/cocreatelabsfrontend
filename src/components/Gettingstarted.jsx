1aca2221cd52700807b93775ec3a00041b0081b5
Anoop Pandey <75595666+gargdev@users.noreply.github.com>
2023-07-13 13:58:59 +0530

    Update Gettingstarted.jsx

--- src/components/Gettingstarted.jsx
+++ src/components/Gettingstarted.jsx
@@ -1,36 +1,3 @@
-// import React from "react";
-// import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
-// import "./gettingstarted.css";
-// import rightarrow from "../assets/line.png";
-
-// const Gettingstarted = () => {
-//   return (
-//     <div>
-//       <div className="section1">
-//         <div className="getstarted-content">
-//           <div className="text-section">
-//             <h1>Find Your Next Dream Connect</h1>
-//             <br />
-//             <h2>Get started with us Sign up Today!</h2>
-//           </div>
-//           <div className="button-container">
-//             <button id="btn1">
-//               Sign Up{" "}
-//               <span>
-//                 <img src={rightarrow} alt="" />
-//               </span>
-//             </button>
-//             <button id="btn2">Get Free Credit</button>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Gettingstarted;
-
-
 import React from "react";
 import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
 import "./gettingstarted.css";
