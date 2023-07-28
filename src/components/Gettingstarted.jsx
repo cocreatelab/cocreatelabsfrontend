29be1aad0d02e4bbbc19541dc075e93ef79e7160
Anoop Pandey <75595666+gargdev@users.noreply.github.com>
2023-07-28 16:04:34 +0530

    Update Gettingstarted.jsx

--- src/components/Gettingstarted.jsx
+++ src/components/Gettingstarted.jsx
@@ -5,18 +5,18 @@ import rightarrow from "../assets/line.png";
 
 const Gettingstarted = () => {
   return (
-    <div className="bg-gray-200">
-      <div className="section1 flex justify-center items-center">
+    <div className="container-fluid">
+      <div className="section1">
         <div className="getstarted-content">
           <div className="text-section">
-            <h1 className="text-4xl font-bold">Find Your Next Dream Connect</h1>
+            <h1 className="">Find Your Next Dream Connect</h1>
             <br />
-            <h2 className="text-2xl">Get started with us Sign up Today!</h2>
+            <h2 className="">Get started with us Sign up Today!</h2>
           </div>
           <div className="button-container mt-8">
             <button
               id="btn1"
-              className="bg-blue-500 text-black rounded-md mr-4"
+              class="bg-blue-500 text-black rounded-md mr-4"
             >
               Sign Up{" "}
               <span>
@@ -25,7 +25,7 @@ const Gettingstarted = () => {
             </button>
             <button
               id="btn2"
-              className="bg-green-500 text-black rounded-md"
+              class="bg-green-500 text-black rounded-md"
             >
               Get Free Credit
             </button>
