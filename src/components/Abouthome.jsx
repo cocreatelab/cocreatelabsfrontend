c7e632c1007923b345d8958f3e019c0a8ff8c625
Anoop Pandey <75595666+gargdev@users.noreply.github.com>
2023-07-28 15:41:34 +0530

    Update Abouthome.jsx

--- src/components/Abouthome.jsx
+++ src/components/Abouthome.jsx
@@ -1,66 +1,3 @@
-// import React from "react";
-// import "./abouthome.css";
-// import Aboutbanner from "../assets/Aboutbanner.png";
-// import People from "../assets/People.png";
-// import Fist from "../assets/Fist.png";
-
-// function Abouthome() {
-//   return (
-//     <>
-//       <div className="about-header-parent-conatiner">
-//         <div className="about-header-left-container">
-//           <div className="about-home-main-heading">
-//             <h1>
-//               About CCL
-//             </h1>
-//             <h4>A game-changer in the startup ecosystem</h4>
-//           </div>
-//           <div className="about-home-primary-text">
-//             <p>
-//               It enables access to a global platform that brings together the
-//               best and brightest entrepreneurs, investors, mentors, and
-//               philanthropic organizations in an integrated ecosystem.
-//             </p>
-//             <p>
-//               With a network of world-class mentors from companies like Meta,
-//               Netflix, Uber, Shell, and Harvard University, along with the World
-//               Bank and OECD, and other multi-laterals, Co Create Labs is set to
-//               revolutionize the way start-ups are launched and grown.
-//             </p>
-//           </div>
-//           <div about-home-apply-btn>
-//             <button className="apply-btn">Apply Now</button>
-//           </div>
-//           <div className="about-slogan-conatiner">
-//             <div className="about-slogan">
-//               <img src={Fist} alt="" />
-//               <h3>EMPOWER</h3>
-//               <p>
-//                 Empowering the world’s best entrepreneurs through all stages of
-//                 their growth with mentorship, funds and services.
-//               </p>
-//             </div>
-//             <div className="about-slogan">
-//               <img src={People} alt="" />
-//               <h3>COMMUNITY</h3>
-//               <p>
-//                 Connecting the investors with a curated list of impactful &
-//                 investment ready startups.
-//               </p>
-//             </div>
-//           </div>
-//         </div>
-//         <div className="about-header-right-container">
-//           <img src={Aboutbanner} alt="" />
-//         </div>
-//       </div>
-//     </>
-//   );
-// }
-
-// export default Abouthome;
-
-
 import React from "react";
 import "./abouthome.css";
 import Aboutbanner from "../assets/Aboutbanner.png";
@@ -71,21 +8,25 @@ function Abouthome() {
   return (
     <>
       <div className="about-header-parent-conatiner">
-        <div className="about-header-left-container">
-          <div className="about-home-main-heading">
+        <div className="container about-header-left-container">
+          <div className="container about-home-main-heading">
             <h1 className="text-4xl lg:text-6xl">
               About CCL
             </h1>
             <h4 className="text-lg lg:text-xl">A game-changer in the startup ecosystem</h4>
           </div>
-          <div className="about-home-primary-text">
-            <p className="text-sm lg:text-base">
+          <div className="container about-header-right-container1">
+          <img src={Aboutbanner} alt="" className="w-full" />
+        </div>
+          <div className="container about-home-primary-text">
+            <p className="">
               It enables access to a global platform that brings together the
               best and brightest entrepreneurs, investors, mentors, and
               philanthropic organizations in an integrated ecosystem.
             </p>
               <br />
-            <p className="text-sm lg:text-base">
+              <br />
+            <p className="">
               With a network of world-class mentors from companies like Meta,
               Netflix, Uber, Shell, and Harvard University, along with the World
               Bank and OECD, and other multi-laterals, Co Create Labs is set to
@@ -93,13 +34,13 @@ function Abouthome() {
             </p>
           </div>
           <div className="about-home-apply-btn">
-            <button className="apply-btn text-white py-4 px-5 rounded-full">
+            <button className="apply-btn">
               Apply Now
             </button>
           </div>
-          <div className="about-slogan-conatiner">
+          <div className="container about-slogan-conatiner">
             <div className="about-slogan">
-              <img src={Fist} alt="" className="w-8 h-8 lg:w-12 lg:h-12" />
+              <img src={Fist} alt="" className="" class="img-fluid" />
               <h3 className="text-xl lg:text-2xl">EMPOWER</h3>
               <p className="text-sm lg:text-base">
                 Empowering the world’s best entrepreneurs through all stages of
@@ -107,7 +48,7 @@ function Abouthome() {
               </p>
             </div>
             <div className="about-slogan">
-              <img src={People} alt="" className="w-8 h-8 lg:w-12 lg:h-12" />
+              <img src={People} alt="" className="" class="img-fluid" />
               <h3 className="text-xl lg:text-2xl">COMMUNITY</h3>
               <p className="text-sm lg:text-base">
                 Connecting the investors with a curated list of impactful &
@@ -116,8 +57,8 @@ function Abouthome() {
             </div>
           </div>
         </div>
-        <div className="about-header-right-container">
-          <img src={Aboutbanner} alt="" className="w-full" />
+        <div className="container about-header-right-container">
+          <img src={Aboutbanner} alt="" className="w-full"/>
         </div>
       </div>
     </>
