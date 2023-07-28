9e7cca6be44359bee1f9db40408fd7a82fb04565
Anoop Pandey <75595666+gargdev@users.noreply.github.com>
2023-07-28 15:44:36 +0530

    Update Mission.jsx

--- src/components/Mission.jsx
+++ src/components/Mission.jsx
@@ -1,18 +1,17 @@
 import React from "react";
 import "./mission.css";
-import mission from "../assets/mission.png";
-import vision from "../assets/vision.png";
+import mission from "../assets/mission (2).png";
+import vision from "../assets/vision (2).png";
 
 const Mission = () => {
   return (
     <>
       <div className="mission-parent-container">
         <div className="mission-content-container">
-          <div className="mission-content">
-            <div className="mission-left-content">
-              <b>
-                <h6>Our Mission </h6>
-              </b>
+
+          <div className="container mission-content">
+            <div className="container mission-left-content">
+              <h6>Our Mission </h6>
               <h3>Inspire, Innovate, Share</h3>
               <p>
                 Building a thriving community of mentors, investors,
@@ -21,26 +20,26 @@ const Mission = () => {
                 Connect, Investor Connect & Grants Connect.
               </p>
             </div>
-            <div className="mission-right-content">
-              <img src={mission} alt="" />
+            <div className="container mission-right-content">
+              <img src={mission} alt="" class="img-fluid" />
             </div>
           </div>
 
-          <div className="mission-content">
-            <div className="mission-left-content1">
+          <div className="container mission-content">
+            <div className="container mission-left-content1">
               <img src={vision} alt="" />
             </div>
-            <div className="mission-right-content1">
-              <b>
-                <h6>Our Mission </h6>
-              </b>
-              <h3>Inspire, Innovate, Share</h3>
+            <div className="container mission-right-content1">
+              <h6>Our Vision</h6>
+              <h3>Laser focus</h3>
               <p>
-                Building a thriving community of mentors, investors,
-                entrepreneurs, startups, evangelists and nonprofits through our
-                integrated platform vis-à-vis 3 verticals of our company: Mentor
-                Connect, Investor Connect & Grants Connect.
+                Democratizing access to global expertise and funding
+                opportunities for aspiring entrepreneurs, startups, and
+                nonprofits.
               </p>
+            </div>
+            <div className="container mission-left-content12">
+              <img src={vision} alt="" />
             </div>
           </div>
         </div>
