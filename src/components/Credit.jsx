17b7725d67ba8e12b208e2ed6074a8937d494a89
Anoop Pandey <75595666+gargdev@users.noreply.github.com>
2023-07-28 15:45:37 +0530

    Update Credit.jsx

--- src/components/Credit.jsx
+++ src/components/Credit.jsx
@@ -4,38 +4,55 @@ import Iphone from "../assets/iPhones.png";
 import teamimg from "../assets/teamimg.png"
 const Credit = () => {
   return (
-    <div className="credit-parent-container">
-      <div className="credit-left-container">
-        <div className="credit-heading-left-container">
+    <div className="container credit-parent-container">
+      <div className="container credit-left-container">
           <h3>
             We recognize that your <wrap>time</wrap> is the most expensive
             capital in your startup
           </h3>
-        </div>
-        <div className="credit-para-left-container">
           <p>
             We introduce a unique credit system so that all this is accessible
             with ease to the busy founding team.
           </p>
-        </div>
-        <div className="credit-icon-left-container">
+        
+        <div className="container credit-icon-left-container">
           <div className="credit-icon1-left-container">
-            <p>✔&nbsp; &nbsp; CCL Credits</p>
-            <p>&nbsp;&nbsp;✔&nbsp; &nbsp;  Easy to use</p>
+            <p>✔ CCL Credits</p>
+            <p>✔ Easy to use</p>
           </div>
           <div className="credit-icon2-left-container">
-            <p>✔&nbsp; &nbsp; Access (global experts)</p>
-            <p>✔&nbsp; &nbsp;&nbsp;No hidden cost</p>
+            <p>✔ Access (global experts)</p>
+            <p>✔ No hidden cost</p>
           </div>
         </div>
-        <div className="credit-para2-left-container">
-          <p>
+        
+          <p className="text1">
             Buy credit with Co-Create Labs and pre book sessions with leaders
             across the globe and begin your entrepreneurial journey with
             guidance & validation!
           </p>
+        
+        <div className="credit-button-left-container">
+          
+            <button type="submit" className="Apply-Now rounded-full">
+              Apply Now
+            </button>
+          
+          <a href="#/"  className="font-medium text-base leading-6 text-center text-blue-500">
+            Avail Free Credits
+            </a>
         </div>
-        <div className="credit-button-left-container flex gap-3">
+      </div>
+      <div className="container credit-right-container">
+        <img src={Iphone} alt="" class="img-fluid" />
+
+        <p className="text2">
+            Buy credit with Co-Create Labs and pre book sessions with leaders
+            across the globe and begin your entrepreneurial journey with
+            guidance & validation!
+          </p>
+        
+        <div className="credit-button-left-container1">
           <form className="apply-button">
             <button type="submit" className="Apply-Now rounded-full">
               Apply Now
@@ -46,12 +63,11 @@ const Credit = () => {
             </a>
         </div>
       </div>
-      <div className="credit-right-container">
-        <img src={Iphone} alt="" className="iphone-icon" />
-      </div>
+
+
+      
     </div>
   );
 };
 
 export default Credit;
-
