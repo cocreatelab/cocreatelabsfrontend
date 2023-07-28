ac2223f93662b0dfdfe13e108fc8476fc97119c5
Anoop Pandey <75595666+gargdev@users.noreply.github.com>
2023-07-28 16:06:34 +0530

    Update Joinnow.jsx

--- src/components/Joinnow.jsx
+++ src/components/Joinnow.jsx
@@ -3,11 +3,12 @@ import "./joinnow.css";
 import Iphoneimg from "../assets/iphoneimg.png";
 import Greenellipse from "../assets/greenellipse.png";
 import Blueellipse from "../assets/blueellipse.png";
+import iphoneimg from "../assets/joinimgphone.png"
 
 const Joinnow = () => {
   return (
     <div>
-      <div className="joinnow-parent-container">
+      <div className="conatainer joinnow-parent-container">
         <div className="joinnow-background">
           <p></p>
         </div>
@@ -15,7 +16,7 @@ const Joinnow = () => {
           <div className="joinnow-rectangle-content">
             <div className="joinnow-rectangle-content-left">
               <div>
-                <p className="joinnow-primary-heading">
+                <p className="container joinnow-primary-heading">
                   Join now & Become member Apply For Credit !
                 </p>
                 <br />
@@ -47,11 +48,13 @@ const Joinnow = () => {
               <div className="iphone-img">
                 <img src={Iphoneimg} alt="" />
               </div>
+              {/* <img src={iphoneimg} alt="" class="img-fluid" className="mr-60"/> */}
             </div>
           </div>
         </div>
       </div>
     </div>
+    
   );
 };
 
